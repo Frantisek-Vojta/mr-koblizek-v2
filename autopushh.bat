3c5efc70c553028dca3f81c646a9f6c0196392f1
František Vojta <frantisek.vojta@orbiszlin.cz>
2025-03-11 19:41:21 +0100

    commit - 11.03.2025--_

--- autopushh.bat
+++ autopushh.bat
@@ -1,55 +1,20 @@
 @echo off
 cd /d "C:\Users\xCel_\Desktop\mr-koblih-v5312789"
 
-:: Zkontroluj vzdálený repozitář
-echo Kontroluji vzdálený repozitář...
-git remote -v
-
-:: Zkontroluj aktuální větev
-echo Kontroluji aktuální větev...
-git branch
-
-:: Zkontroluj změny v repozitáři
-echo Zkontroluji změny v repozitáři...
-git status
-
-:: Pokud jsou nějaké změny, požádej o commit zprávu
-if not exist ".git" (
-    echo Tento adresar neni git repozitar!
-    pause
-    exit /b
-)
-
-set /p commitMessage="Zadejte název pro commit: "
-
 :: Získání aktuálního data a času pro commit zprávu
 for /f "tokens=1-4 delims=/- " %%a in ("%date%") do set currentDateTime=%%a-%%b-%%c_%%d
 
-:: Spojení commit zprávy s aktuálním datem a časem
-set finalCommitMessage=%commitMessage% - %currentDateTime%
+:: Nastavení commit zprávy s názvem "commit" a aktuálním datem a časem
+set finalCommitMessage=commit - %currentDateTime%
 
-:: Přidání změn do Git a commit
-echo Přidávám změny do Gitu...
+:: Přidání změn do Gitu
 git add .
+
+:: Commit s automaticky generovanou zprávou
 git commit -m "%finalCommitMessage%"
 
-:: Kontrola, zda commit byl úspěšný
-if %errorlevel% neq 0 (
-    echo Commit selhal! Zkontrolujte chyby.
-    pause
-    exit /b
-)
-
-:: Pushnutí změn do vzdáleného repozitáře
-echo Pushuji změny do origin/main...
+:: Pushnutí změn na origin/main
 git push origin main
-
-:: Zkontroluj, zda push proběhl úspěšně
-if %errorlevel% neq 0 (
-    echo Push selhal! Zkontrolujte chyby.
-    pause
-    exit /b
-)
 
 :: Úspěšné dokončení
 echo Commit a push byl úspěšně dokončen.
