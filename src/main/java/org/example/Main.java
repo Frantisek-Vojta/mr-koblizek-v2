b9a45fd9f2924ccce32cabd30abfcd85dc4390f7
Frantisek-vojta <vojta.fran@gmail.com>
2025-03-21 14:58:36 +0100

    gu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,6 +19,10 @@ import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.Random;
 
+
+
+
+
 public class Main extends ListenerAdapter {
 
     private static final Path IMG_KOBLIZEK = Paths.get("imgs/koblizek");
