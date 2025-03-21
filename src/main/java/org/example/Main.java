9af7cad0fae4ceb01aeecda1610f06bc8b5fc9fb
Frantisek-vojta <vojta.fran@gmail.com>
2025-03-21 20:23:46 +0100

    ads 21.03.2025 20:23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,22 +2,18 @@ package org.example;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel; // Ensure this import is present
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.utils.FileUpload;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
-
-
-
-
-
-
-// funguje uz ten autopush?
-import javax.swing.text.html.Option;
+import javax.security.auth.login.LoginException;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,73 +21,23 @@ import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.Random;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 public class Main extends ListenerAdapter {
 
+    private static final String STAFF_ROLE_ID = "1325367756182257674"; // ID role to check
     private static final Path IMG_KOBLIZEK = Paths.get("imgs/koblizek");
     private static final Path IMG_MEME = Paths.get("imgs/meme");
     private static final Path IMG_NITRO = Paths.get("imgs/nitro");
 
-    // Metoda pro načtení tokenu ze souboru
+    // Method to load the token from a file
     public static String loadToken(String filePath) {
         Properties properties = new Properties();
         try (FileInputStream input = new FileInputStream(filePath)) {
             properties.load(input);
-            return properties.getProperty("token", ""); // Vrátí token nebo prázdný string, pokud není nalezen
+            return properties.getProperty("token", ""); // Returns token or empty string if not found
         } catch (IOException e) {
-            System.err.println("Chyba při načítání souboru: " + e.getMessage());
+            System.err.println("Error loading file: " + e.getMessage());
             return null;
         }
-    }
-
-
-
-
-
-// komentar je na to jestli funguje --force push xd xd xd
-
-
-    public static void main(String[] args) throws InterruptedException{
-        // Načtení tokenu ze souboru
-        String token = loadToken("src/config.properties");
-
-
-
-
-        // Inicializace JDA
-        JDA jda = JDABuilder.createDefault(token)
-                .addEventListeners(new Main())
-                .build()
-                .awaitReady();
-
-        // Nastavení příkazů pro bota
-        jda.updateCommands()
-                .addCommands(
-                        Commands.slash("koblizek", "Send random image of donut"),
-                        Commands.slash("meme", "Send random very funny meme"),
-                        Commands.slash("freenitro", "Give you free nitro frfr no scam 100% working"),
-                        Commands.slash("botinfo", "Send info about me"),
-                        Commands.slash("help", "Show help"),
-                        Commands.slash("ping", "Check bot's latency"),
-                        Commands.slash("idk", "Send shrug emoji"),
-                        Commands.slash("guess", "Guess the numbeer" )
-                                .addOption(OptionType.INTEGER, "number", "Your guess 1 or 2", true)
-                )
-                .queue();
     }
 
     @Override
@@ -99,7 +45,7 @@ public class Main extends ListenerAdapter {
         String command = event.getName();
         MessageChannel channel = event.getMessageChannel();
 
-        // Příkaz koblizek
+        // Command koblizek
         if (command.equals("koblizek")) {
             try {
                 sendRandomImage(channel, IMG_KOBLIZEK, "🍩 Random photo of koblizek 🍩", "Here is your random koblizek image!", event);
@@ -107,8 +53,7 @@ public class Main extends ListenerAdapter {
                 throw new RuntimeException(e);
             }
         }
-
-        // Příkaz meme
+        // Command meme
         else if (command.equals("meme")) {
             try {
                 sendRandomImage(channel, IMG_MEME, "😂 Random meme 😂", "Here is your random meme!", event);
@@ -116,8 +61,7 @@ public class Main extends ListenerAdapter {
                 throw new RuntimeException(e);
             }
         }
-
-        // Příkaz free nitro
+        // Command free nitro
         else if (command.equals("freenitro")) {
             try {
                 sendRandomImage(channel, IMG_NITRO, ":rocket: Here is your free nitro :rocket:", "No scam 100% working no virus frfr", event);
@@ -125,8 +69,7 @@ public class Main extends ListenerAdapter {
                 throw new RuntimeException(e);
             }
         }
-
-        // Příkaz botinfo
+        // Command botinfo
         else if (command.equals("botinfo")) {
             EmbedBuilder embed = new EmbedBuilder()
                     .setTitle("<:icon:1347146418049122315> Bot Info <:icon:1347146418049122315>")
@@ -139,8 +82,7 @@ public class Main extends ListenerAdapter {
                     .setColor(0xfcb603);
             event.replyEmbeds(embed.build()).queue();
         }
-
-        // Příkaz help
+        // Command help
         else if (command.equals("help")) {
             EmbedBuilder embed = new EmbedBuilder()
                     .setTitle("====== Help ======")
@@ -154,67 +96,85 @@ public class Main extends ListenerAdapter {
                     .setColor(0xfcb603)
                     .setFooter("I don't know what to add? DM me for tips plz");
             event.replyEmbeds(embed.build()).queue();
+        }
+        // Command guess
+        else if (command.equals("guess")) {
+            String[] parts = event.getOptions().get(0).getAsString().split("\\s+");
 
+            if (parts.length != 1) {
+                event.replyEmbeds(new EmbedBuilder()
+                        .setDescription("Please use the command like: /guess 1 or /guess 2")
+                        .setColor(0xfcb603)
+                        .build()).queue();
+                return;
+            }
 
-            // funguje auto commit? ano nebo ne
-
-        } else if (command.equals("guess")) {
-                String[] parts = event.getOptions().get(0).getAsString().split("\\s+");
-
-                if (parts.length != 1) {
+            try {
+                int guess = Integer.parseInt(parts[0]);
+                if (guess < 1 || guess > 2) {
                     event.replyEmbeds(new EmbedBuilder()
-                            .setDescription("Please use the command like: /guess 1 or /guess 2")
+                            .setDescription("The number must be either 1 or 2!")
                             .setColor(0xfcb603)
                             .build()).queue();
                     return;
                 }
 
-                try {
-                    int guess = Integer.parseInt(parts[0]);
-                    if (guess < 1 || guess > 2) {
-                        event.replyEmbeds(new EmbedBuilder()
-                                .setDescription("The number must be either 1 or 2!")
-                                .setColor(0xfcb603)
-                                .build()).queue();
-                        return;
-                    }
+                int random = (int) (Math.random() * 2) + 1;
 
-                    int random = (int) (Math.random() * 2) + 1;
-
-                    EmbedBuilder embed = new EmbedBuilder();
-                    if (guess == random) {
-                        embed.setDescription("🎉 **OMG! You won!** 🎉")
-                                .setColor(0x00FF00);
-                    } else {
-                        embed.setDescription("😔 **Bruh! You lost, lil bro** 😔")
-                                .setColor(0xFF0000);
-                    }
-                    event.replyEmbeds(embed.build()).queue();
-                } catch (NumberFormatException e) {
-                    event.replyEmbeds(new EmbedBuilder()
-                            .setDescription("Please provide a valid number: 1 or 2!")
-                            .setColor(0xfcb603)
-                            .build()).queue();
+                EmbedBuilder embed = new EmbedBuilder();
+                if (guess == random) {
+                    embed.setDescription("🎉 **OMG! You won!** 🎉")
+                            .setColor(0x00FF00);
+                } else {
+                    embed.setDescription("😔 **Bruh! You lost, lil bro** 😔")
+                            .setColor(0xFF0000);
                 }
+                event.replyEmbeds(embed.build()).queue();
+            } catch (NumberFormatException e) {
+                event.replyEmbeds(new EmbedBuilder()
+                        .setDescription("Please provide a valid number: 1 or 2!")
+                        .setColor(0xfcb603)
+                        .build()).queue();
             }
-
-
-
-            // Příkaz ping
+        }
+        // Command ping
         else if (command.equals("ping")) {
             long latency = event.getJDA().getGatewayPing();
             EmbedBuilder embed = new EmbedBuilder()
                     .setTitle("Ping")
-                    .setDescription("Lateency: " + latency + " ms")
+                    .setDescription("Latency: " + latency + " ms")
                     .setColor(0xfcb603);
 
             event.replyEmbeds(embed.build()).queue();
         }
     }
 
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        Message message = event.getMessage();
+        MessageChannel channel = message.getChannel();
+
+        // Check if any users were mentioned in the message
+        message.getMentions().getUsers().forEach(user -> {
+            // Retrieve the member from the guild
+            event.getGuild().retrieveMember(user).queue(member -> {
+                // If the user has the specified role, send a warning
+                if (member.getRoles().stream().anyMatch(role -> role.getId().equals(STAFF_ROLE_ID))) {
+                    EmbedBuilder embed = new EmbedBuilder()
+                            .setTitle("Warning")
+                            .setDescription("Do not ping the ** staff ** members please :pleading_face:")
+                            .setFooter("soon")
+                            .setColor(0xFF0000); // Red color for warning
+
+                    channel.sendMessageEmbeds(embed.build()).queue();
+                }
+            });
+        });
+    }
+
     private void sendRandomImage(MessageChannel channel, Path directory, String title, String description, SlashCommandInteractionEvent event) throws IOException {
         if (!Files.exists(directory) || Files.list(directory).count() == 0) {
-            event.reply("There are no images available.");
+            event.reply("There are no images available.").queue();
             return;
         }
         Random random = new Random();
@@ -226,13 +186,34 @@ public class Main extends ListenerAdapter {
                     .setDescription(description)
                     .setColor(0xfcb603)
                     .setImage("attachment://" + image.getName());
+
+            // Use addFile instead of addFiles
             event.replyEmbeds(embed.build())
-                    .addFiles(FileUpload.fromData(image, image.getName())).queue();
+                    .addFile(image) // Use addFile for a single file
+                    .queue();
         }
+    }
 
+    public static void main(String[] args) throws InterruptedException, LoginException {
+        String token = loadToken("src/config.properties");
 
+        JDA jda = JDABuilder.createDefault(token)
+                .addEventListeners(new Main())
+                .build()
+                .awaitReady();
 
-
-
+        jda.updateCommands()
+                .addCommands(
+                        Commands.slash("koblizek", "Send random image of donut"),
+                        Commands.slash("meme", "Send random very funny meme"),
+                        Commands.slash("freenitro", "Give you free nitro frfr no scam 100% working"),
+                        Commands.slash("botinfo", "Send info about me"),
+                        Commands.slash("help", "Show help"),
+                        Commands.slash("ping", "Check bot's latency"),
+                        Commands.slash("idk", "Send shrug emoji"),
+                        Commands.slash("guess", "Guess the number")
+                                .addOption(OptionType.INTEGER, "number", "Your guess 1 or 2", true)
+                )
+                .queue();
     }
 }
